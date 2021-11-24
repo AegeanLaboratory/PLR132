0a9d02ba0bff837613394ec6a0bc38b691660dfc
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-11-24 18:39:45 +0100

    Update 1_ctxxxxx.c

--- ergasies/source/1_ctxxxxx.c
+++ ergasies/source/1_ctxxxxx.c
@@ -15,13 +15,10 @@ onoma mas kai to epitheto mas opos episis kai to CT mas.
 int main()
 {
     int apo, ews, etos, hmera;
-
     printf("Arxi etos: ");
     scanf("%d", &apo);
-
     printf("Telos etos: ");
     scanf("%d", &ews);
-
     for (etos = apo; etos <= ews; etos++)
     {
         int a = etos % 19;
@@ -29,17 +26,13 @@ int main()
         int g = etos % 7;
         int d = (19 * a + 16) % 30;
         int e = (2 * b + 4 * g + 6 * d) % 7;
-
         hmera = d + e + 3;
-
         if (hmera <= 30)
             printf("Pasxa %d -> %d Apriliou\n", etos, hmera);
         else
             printf("Pasxa %d -> %d Maiou\n", etos, (hmera - 30));
     }
-
     printf(NAME "\n");
     printf(CT "\n");
-
     return 0;
 }
