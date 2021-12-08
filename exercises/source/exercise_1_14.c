da736b64708c4d049034f211a93fece3abf1c0b5
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 11:08:28 +0100

    Update exercise_1_14.c

--- exercises/source/exercise_1_14.c
+++ exercises/source/exercise_1_14.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	printf("Xaraktires me ASCII kwdika:\n");
-	printf("80 = %c\n65 = %c\n66 = %c\n47 = %c", 80, 65, 66, 47);
-	
-	return 0;
+    printf("Xaraktires me ASCII kwdika:\n");
+    printf("80 = %c\n65 = %c\n66 = %c\n47 = %c", 80, 65, 66, 47);
+
+    return 0;
 }
