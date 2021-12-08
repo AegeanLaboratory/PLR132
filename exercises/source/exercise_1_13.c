f2e7d187ae57dc73aa4075b23b53f06d99dca7ab
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 11:06:52 +0100

    Update exercise_1_13.c

--- exercises/source/exercise_1_13.c
+++ exercises/source/exercise_1_13.c
@@ -5,26 +5,25 @@
 #define T 0.15
 
 int main(){
-	
-	float poso;
-	char eidos;
-	
-	printf("Posa grammatosima theleis kai poio eidos (E-A-T)? ");
-	scanf("%f %c", &poso, &eidos);
-	
-	switch(eidos)
-	{
-		case 'E':
-			printf("Me to eidos %c tha plirwseis %.2f.", eidos, (poso*E));
-			break;
-		case 'A':
-			printf("Me to eidos %c tha plirwseis %.2f.", eidos, (poso*A));
-			break;
-		case 'T':
-			printf("Me to eidos %c tha plirwseis %.2f.", eidos, (poso*T));
-			break;
-		default:
-			printf("To eidos pou zitas den yparxei!");
-	}
-	return 0;
+    float poso;
+    char eidos;
+
+    printf("Posa grammatosima theleis kai poio eidos (E-A-T)? ");
+    scanf("%f %c", &poso, &eidos);
+
+    switch(eidos)
+    {
+        case 'E':
+            printf("Me to eidos %c tha plirwseis %.2f.", eidos, (poso*E));
+            break;
+        case 'A':
+            printf("Me to eidos %c tha plirwseis %.2f.", eidos, (poso*A));
+            break;
+        case 'T':
+            printf("Me to eidos %c tha plirwseis %.2f.", eidos, (poso*T));
+            break;
+        default:
+            printf("To eidos pou zitas den yparxei!");
+    }
+    return 0;
 }
