f05b63e1075437dd82097a13a186e21521de0464
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 10:42:21 +0100

    Update exercise_1_01.c

--- exercises/source/exercise_1_01.c
+++ exercises/source/exercise_1_01.c
@@ -2,21 +2,21 @@
 
 int main()
 {
-	int num1, num2, temp;
+    int num1, num2, temp;
 
-	printf("Dwse enan arithmo: ");
-	scanf("%d", &num1);
+    printf("Dwse enan arithmo: ");
+    scanf("%d", &num1);
 
-	printf("Dwse akoma enan arithmo: ");
-	scanf("%d", &num2);
+    printf("Dwse akoma enan arithmo: ");
+    scanf("%d", &num2);
 
-	printf("\nPrin tin antimetathesi\nnum1 = %d\nnum2 = %d", num1, num2);
+    printf("\nPrin tin antimetathesi\nnum1 = %d\nnum2 = %d", num1, num2);
 
-	temp = num1;
-	num1 = num2;
-	num2 = temp;
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
 
-	printf("\n\nMeta tin antimetathesi\nnum1 = %d\nnum2 = %d", num1, num2);
+    printf("\n\nMeta tin antimetathesi\nnum1 = %d\nnum2 = %d", num1, num2);
 
-	return 0;
+    return 0;
 }
