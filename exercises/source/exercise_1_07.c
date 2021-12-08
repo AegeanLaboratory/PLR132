5a88e0b17ed0d495d0d4db3d0aff692650fa3484
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 10:51:12 +0100

    Update exercise_1_07.c

--- exercises/source/exercise_1_07.c
+++ exercises/source/exercise_1_07.c
@@ -2,53 +2,52 @@
 
 int main()
 {
-	
-	int month;
-	
-	printf("Poio mina thes na deis (1-12)? ");
-	scanf("%d", &month);
-	
-	switch(month){
-		case 1:
-			printf("O %dos minas einai o Ianouarios kai exei 31 meres.", month);
-			break;
-		case 2:
-			printf("O %dos minas einai o Febrouarios kai exei 28 meres.", month);
-			break;
-		case 3:
-			printf("O %dos minas einai o Martios kai exei 31 meres.", month);
-			break;
-		case 4:
-			printf("O %dos minas einai o Aprilios kai exei 30 meres.", month);
-			break;
-		case 5:
-			printf("O %dos minas einai o Maios kai exei 31 meres.", month);
-			break;
-		case 6:
-			printf("O %dos minas einai o Iounios kai exei 30 meres.", month);
-			break;
-		case 7:
-			printf("O %dos minas einai o Ioulios kai exei 31 meres.", month);
-			break;
-		case 8:
-			printf("O %dos minas einai o Augoustos kai exei 31 meres.", month);
-			break;
-		case 9:
-			printf("O %dos minas einai o Septemvrios kai exei 30 meres.", month);
-			break;
-		case 10:
-			printf("O %dos minas einai o Oktovrios kai exei 31 meres.", month);
-			break;
-		case 11:
-			printf("O %dos minas einai o Noemvrios kai exei 30 meres.", month);
-			break;
-		case 12:
-			printf("O %dos minas einai o Dekemvrios kai exei 31 meres.", month);
-			break;
-		default:
-			printf("Kati pige lathos!");
-			break;
-	}
+    int month;
 
-	return 0;
+    printf("Poio mina thes na deis (1-12)? ");
+    scanf("%d", &month);
+
+    switch(month){
+        case 1:
+            printf("O %dos minas einai o Ianouarios kai exei 31 meres.", month);
+            break;
+        case 2:
+            printf("O %dos minas einai o Febrouarios kai exei 28 meres.", month);
+            break;
+        case 3:
+            printf("O %dos minas einai o Martios kai exei 31 meres.", month);
+            break;
+        case 4:
+            printf("O %dos minas einai o Aprilios kai exei 30 meres.", month);
+            break;
+        case 5:
+            printf("O %dos minas einai o Maios kai exei 31 meres.", month);
+            break;
+        case 6:
+            printf("O %dos minas einai o Iounios kai exei 30 meres.", month);
+            break;
+        case 7:
+            printf("O %dos minas einai o Ioulios kai exei 31 meres.", month);
+            break;
+        case 8:
+            printf("O %dos minas einai o Augoustos kai exei 31 meres.", month);
+            break;
+        case 9:
+            printf("O %dos minas einai o Septemvrios kai exei 30 meres.", month);
+            break;
+        case 10:
+            printf("O %dos minas einai o Oktovrios kai exei 31 meres.", month);
+            break;
+        case 11:
+            printf("O %dos minas einai o Noemvrios kai exei 30 meres.", month);
+            break;
+        case 12:
+            printf("O %dos minas einai o Dekemvrios kai exei 31 meres.", month);
+            break;
+        default:
+            printf("Kati pige lathos!");
+            break;
+    }
+
+    return 0;
 }
