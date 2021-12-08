b032b25a3e27142c65b5e4a1dd4bbc359d01be91
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 10:48:51 +0100

    Update exercise_1_05.c

--- exercises/source/exercise_1_05.c
+++ exercises/source/exercise_1_05.c
@@ -2,29 +2,29 @@
 
 int main()
 {
-	int num1, num2, num3;
+    int num1, num2, num3;
 
-	printf("Dwse ton 1o arithmo: ");
-	scanf("%d", &num1);
+    printf("Dwse ton 1o arithmo: ");
+    scanf("%d", &num1);
 
-	printf("Dwse ton 2o arithmo: ");
-	scanf("%d", &num2);
+    printf("Dwse ton 2o arithmo: ");
+    scanf("%d", &num2);
 
-	printf("Dwse ton 3o arithmo: ");
-	scanf("%d", &num3);
+    printf("Dwse ton 3o arithmo: ");
+    scanf("%d", &num3);
 
-	int max = num1;
+    int max = num1;
 
-	if (num2 > max)
-	{
-		max = num2;
-	}
-	if (num3 > max)
-	{
-		max = num3;
-	}
-	
-	printf("O megaluteros arithmos einai to %d.", max);
+    if (num2 > max)
+    {
+        max = num2;
+    }
+    if (num3 > max)
+    {
+        max = num3;
+    }
 
-	return 0;
+    printf("O megaluteros arithmos einai to %d.", max);
+
+    return 0;
 }
