db6086ec37c4b20be6883186743abf5a3bddd328
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 10:55:58 +0100

    Update exercise_1_10.c

--- exercises/source/exercise_1_10.c
+++ exercises/source/exercise_1_10.c
@@ -2,27 +2,26 @@
 
 int main()
 {
+    float eisodima, foros;
 
-	float eisodima, foros;
-	
-	printf("Dwse to eisodima sou: ");
-	scanf("%f", &eisodima);
-	
-	if (eisodima <= 10000)
-	{
-		foros = 0;
-		printf("Me eisodima %.2f o foros einai: %.2f", eisodima, foros);
-	}
-	else if (eisodima <= 15000)
-	{
-		foros = eisodima * 0.1;
-		printf("Me eisodima %.2f o foros einai: %.2f", eisodima, foros);
-	}
-	else
-	{
- 		foros = eisodima * 0.2;
-		printf("Me eisodima %.2f o foros einai: %.2f", eisodima, foros);
-	}
+    printf("Dwse to eisodima sou: ");
+    scanf("%f", &eisodima);
 
-	return 0;
+    if (eisodima <= 10000)
+    {
+        foros = 0;
+        printf("Me eisodima %.2f o foros einai: %.2f", eisodima, foros);
+    }
+    else if (eisodima <= 15000)
+    {
+        foros = eisodima * 0.1;
+        printf("Me eisodima %.2f o foros einai: %.2f", eisodima, foros);
+    }
+    else
+    {
+        foros = eisodima * 0.2;
+        printf("Me eisodima %.2f o foros einai: %.2f", eisodima, foros);
+    }
+
+    return 0;
 }
