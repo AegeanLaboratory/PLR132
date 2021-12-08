cbd6f9c623fbb6ab03cc828340e55ef8ffaf8b0b
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 11:16:00 +0100

    Update and rename exercise_2_6.c to exercise_2_06.c

---
+++ exercises/source/exercise_2_06.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, c1 = 0, c2 = 0;
+
+    do
+    {
+        printf("Dwse arithmo: ");
+        scanf("%d", &num);
+
+        if (num > 0)
+            c1++;
+        else if (num < 0)
+            c2++;
+    } while (num != 0);
+
+    printf("To plithos twn thetikwn arithmwn einai %d.", c1);
+    printf("\nTo plithos twn arnitikwn arithmwn einai %d.", c2);
+
+    return 0;
+}
