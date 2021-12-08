fd28b1f35b52147097e252e6fbb3e7593d82e328
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-08 10:48:09 +0100

    Update exercise_1_04.c

--- exercises/source/exercise_1_04.c
+++ exercises/source/exercise_1_04.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-	int num;
+    int num;
 
-	printf("Dwse arithmo: ");
-	scanf("%d", &num);
+    printf("Dwse arithmo: ");
+    scanf("%d", &num);
 
-	if (num % 2 == 0)
-	{
-		printf("To tetragwno tou arithmou %d einai: %.2f", num, pow(num,2));
-	}
-	else
-	{
-		printf("O kubos tou arithmou %d einai: %.2f", num, pow(num,3));
-	}
+    if (num % 2 == 0)
+    {
+        printf("To tetragwno tou arithmou %d einai: %.2f", num, pow(num,2));
+    }
+    else
+    {
+        printf("O kubos tou arithmou %d einai: %.2f", num, pow(num,3));
+    }
 
-	return 0;
+    return 0;
 }
