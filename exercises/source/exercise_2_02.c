80b5852ec7b4965f3526d483137b5c16ce81c903
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-12 17:09:15 +0100

    Update exercise_2_02.c

--- exercises/source/exercise_2_02.c
+++ exercises/source/exercise_2_02.c
@@ -2,17 +2,16 @@
 
 int main()
 {
+    int i, num, mul = 1;
 
-	int i, num, mul = 1;
-	
-	printf("Dwse enan arithmo: ");
-	scanf("%d", &num);
-	
-	for ( i = 1; i <= num; i++){
-		mul *= i;
-	}
-	
-	printf("To ginomeno twn arithmwn apo to 1 ews kai to %d einai %d.", num, mul);
+    printf("Dwse enan arithmo: ");
+    scanf("%d", &num);
 
-	return 0;
+    for ( i = 1; i <= num; i++){
+        mul *= i;
+    }
+
+    printf("To ginomeno twn arithmwn apo to 1 ews kai to %d einai %d.", num, mul);
+
+    return 0;
 }
