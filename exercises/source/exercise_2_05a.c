afdda8ae522412eefedb5ba04848897170f7098f
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-12 20:35:40 +0100

    Update exercise_2_05a.c

--- exercises/source/exercise_2_05a.c
+++ exercises/source/exercise_2_05a.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+#define START 32
+#define END 128
+
+int main()
+{
+    int i;
+    int j = 0;
+
+    for (i = START; i <= END; i++, j++)
+    {
+        if(!(j % 10) && (j != 0))
+            putchar('\n');
+        printf("%d=%c, ", i, i);
+    }
+
+    return 0;
+}
