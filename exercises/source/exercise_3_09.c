2556acd758b29b1504a6bb9887a644e3d372f970
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-12 20:45:39 +0100

    Update exercise_3_09.c

--- exercises/source/exercise_3_09.c
+++ exercises/source/exercise_3_09.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+float mo(float a, float b);
+
+int main()
+{
+    printf("O mesos oros einai: %.1f", mo(5, 10));
+    return 0;
+}
+
+float mo(float a, float b)
+{
+    float mesos = (a + b) / 2.0;
+}
