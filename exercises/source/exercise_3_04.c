8cf5e8767ba208814a20b1fb8008802dbe6ba537
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-17 10:50:18 +0100

    Update exercise_3_04.c

--- exercises/source/exercise_3_04.c
+++ exercises/source/exercise_3_04.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+void symbols(int n, char ch);
+
+int main()
+{
+    symbols(15, '*');
+    symbols(30, '=');
+    symbols(40, '!');
+
+    return 0;
+}
+
+void symbols(int n, char ch)
+{
+    int i;
+
+    for(i = 1; i <= n; i++)
+    {
+        putchar(ch);
+    }
+    printf("\n");
+}
