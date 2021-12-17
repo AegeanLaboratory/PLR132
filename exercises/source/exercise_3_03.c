ee889144d824d320e93c8b1849f0a30dba4c663f
Efstratia Kazi <efoulakaz@hotmail.com>
2021-12-17 10:49:33 +0100

    Update exercise_3_03.c

--- exercises/source/exercise_3_03.c
+++ exercises/source/exercise_3_03.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int sum(int n);
+
+int main()
+{
+    int athroisma = sum(1000);
+
+    printf("To athroisma apo 1 ews 1000 einai %d.", athroisma);
+
+    return 0;
+}
+
+int sum(int n)
+{
+    int i, athr = 0;
+
+    for (i = 1; i <= n; i++)
+    {
+        athr += i;
+    }
+
+    return athr;
+}
