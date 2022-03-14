845fc6587a43c78b7ef77a2cc04c5fcd546ecc31
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2022-03-14 10:23:57 +0100

    Update lab11_3.c

--- labs/source/lab_11/lab11_3.c
+++ labs/source/lab_11/lab11_3.c
@@ -1,1 +1,24 @@
----
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fp;
+
+    int i;
+    float b1,b2,b3,b4;
+    char onom[30];
+
+    fp=fopen("apot.txt","r");
+
+    while(!feof(fp))
+    {
+        fgets(onom,30,fp);
+        printf("%s",onom);
+        fscanf(fp,"%f %f %f %f ",&b1,&b2,&b3,&b4);
+        printf("%f %f %f %f\n",b1,b2,b3,b4);
+    }
+    fclose(fp);
+
+    return 0;
+}
